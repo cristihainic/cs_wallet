83c3b4223230c53199200079ad0e69e9de72443c
Cristian Hainic <cristihainic@gmail.com>
2017-06-29 18:56:57 +0300

    visual improvements

--- Mine.Designer.cs
+++ Mine.Designer.cs
@@ -29,22 +29,12 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.minedBitcoins = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
-            this.button1.Location = new System.Drawing.Point(107, 30);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(202, 79);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "START";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -60,10 +50,10 @@
             // minedBitcoins
             // 
             this.minedBitcoins.AutoSize = true;
-            this.minedBitcoins.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minedBitcoins.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.minedBitcoins.Location = new System.Drawing.Point(145, 156);
             this.minedBitcoins.Name = "minedBitcoins";
-            this.minedBitcoins.Size = new System.Drawing.Size(57, 25);
+            this.minedBitcoins.Size = new System.Drawing.Size(60, 25);
             this.minedBitcoins.TabIndex = 2;
             this.minedBitcoins.Text = "BTC";
             // 
@@ -73,15 +63,35 @@
             this.timer1.Interval = 4000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "BTC Mining has started!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(54, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(310, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = ">>> Mining with 2 CPU cores, 4 threads <<<";
+            // 
             // Mine
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(414, 369);
             this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.minedBitcoins);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Name = "Mine";
             this.Text = "Mine";
             this.ResumeLayout(false);
@@ -90,10 +100,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label minedBitcoins;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
